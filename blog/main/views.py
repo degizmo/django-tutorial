b558c1f5a2453d0ee8417305e9e9f6874f80dd22
DeGizmo <adam@degizmo.com>
2010-03-22 13:26:20 -0400

    7) our first view

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse
+from main.models import Category
+
+def home(request):
+    cats = Category.objects.all()
+    return HttpResponse(cats)
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+from main.views import *
+
 urlpatterns = patterns('',
     # Example:
     # (r'^blog/', include('blog.foo.urls')),
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^$',                 home),
 )
