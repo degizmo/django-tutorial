9b4ed7bd17bb89d7c5eaec5c6650ce8195df69d0
DeGizmo <adam@degizmo.com>
2010-03-22 13:06:18 -0400

    3) create our models

--- blog/main/models.py
+++ blog/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    Name    =    models.CharField(max_length=255)
+    def __unicode__(self):
+        return self.Name
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+class BlogPost(models.Model):
+	Title		= models.CharField(max_length=255)
+	Category	= models.ForeignKey(Category)
+	Slug		= models.SlugField()
+	Author		= models.ForeignKey(User)
+	PostText	= models.TextField()
+	PublishDate	= models.DateTimeField(auto_now_add=True)
+	def __unicode__(self):
+	    return self.Title
+	    
+admin.site.register(Category)
+admin.site.register(BlogPost)
 
 # Create your models here.
